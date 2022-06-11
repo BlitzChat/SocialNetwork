4866d7a6010db2734b6d2a3844eeb6f715ad378f
AyyItsHamza <105660935+AyyItsHamza@users.noreply.github.com>
2022-06-11 17:27:44 +0200

    fichero dotevn creado

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userModel = require("./user_model");
 const postModel = require("./posts_model");
-const { request, response } = require("express");
 const path = require("path");
+const cors = require('cors');
+const cookieSession = require('cookie-session');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
+app.use(cors)
 app.use(express.static('../frontend'))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+app.use(cookieSession({
+    name: 'blitzchat_session',
+    secret: process.env.SECRET,
+    maxAge: 86400,
+    httpOnly: true
+}))
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DB}.mongodb.net/?retryWrites=true&w=majority`),{
   useNewUrlParser: true,
@@ -24,7 +33,7 @@ db.once("open", function () {
   console.log("Connected successfully");
 });
 
-app.post("/login", async (request, response) => {
+app.post("/api/blitzchat/auth/login", async (request, response) => {
     const user = request.body.username;
     const pass = request.body.password;
 
@@ -33,18 +42,22 @@ app.post("/login", async (request, response) => {
     }  else { 
         const user = await userModel.findOne({username: user});
         if(user === null){
-            response.status(400).json({ msg:"User not found"});
+            return response.status(400).json({ msg:"User not found"});
         } else {
             if(user.password === pass){
-                response.status(200).json({msg:"Login successful"});
+                const token = jwt.sign({
+                    username: user.username,
+                    id: user._id 
+                }, process.env.SECRET, { expiresIn: '1h' });
+                return response.status(200).json({msg:"Login successful"});
             } else {
-                response.status(400).json({msg:"Wrong password"});
+                return response.status(400).json({msg:"Wrong password"});
             }
         }
     }
 });
 
-app.post("/register", async (request, response) => {
+app.post("/api/blitzchat/auth/register", async (request, response) => {
   var user = new userModel({
       nombre : request.body.name,
       username : request.body.username,
@@ -52,19 +65,19 @@ app.post("/register", async (request, response) => {
   });
 
   if(user.nombre === "" || user.username === "" || user.password === ""){
-      response.status(406).json({msg:"Please fill in all fields"});
+      return response.status(406).json({msg:"Please fill in all fields"});
   } else {  // Check if username already exists
       const user = await userModel.findOne({username: user.username});
       if(user === null){
           user.save();
-          response.status(200).json({msg:"User created successfully"});
+          return response.status(200).json({msg:"User created successfully"});
       } else {
-          response.status(400).json({msg:"Username already exists"});
+          return response.status(400).json({msg:"Username already exists"});
       }
   }
 });
 
-app.post("/add_post", async (request, response) => {
+app.post("/api/blitzchat/add_post", async (request, response) => {
   var posts = new postModel({
       text : request.body.text,
       imagen : request.body.imagen,
@@ -73,14 +86,14 @@ app.post("/add_post", async (request, response) => {
   });
 
   if(posts.text === "" ){
-      response.status(406).json({msg: "Please fill atleasat the text field"});
+      return response.status(406).json({msg: "Please fill atleasat the text field"});
   } else { 
       posts.save(); 
-      response.status(200).json({msg: "Post created successfully"});
+      return response.status(200).json({msg: "Post created successfully"});
   } 
 });
 
-app.put("/update_post:id", async (request, response) => {
+app.put("/api/blitzchat/update_post:id", async (request, response) => {
   const id = request.params.id;
   const text = request.body.text;
   const imagen = request.body.imagen;
@@ -88,47 +101,40 @@ app.put("/update_post:id", async (request, response) => {
   const video = request.body.video;
   
   if(id === ""){
-      response.status(406).json({msg: "please enter id of the post"});
+      return response.status(406).json({msg: "please enter id of the post"});
   } else {
       const post = await postModel.findById(id);
       if(post === null){
-          response.status(400).json({msg: "Post not found"});
+          return response.status(400).json({msg: "Post not found"});
       } else {
           post.text = text;
           post.imagen = imagen;
           post.audio = audio;
           post.video = video;
           post.save();
-          response.status(200).json({msg: "Post updated successfully"});
+          return response.status(200).json({msg: "Post updated successfully"});
       }
   }
 });
 
-app.delete("/delete_post:id", async (request, response) => {
+app.delete("/api/blitzchat/delete_post:id", async (request, response) => {
   const id = request.params.id;
   if(id === ""){
-      response.status(406).json({msg: "please enter id of the post"});
+      return response.status(406).json({msg: "please enter id of the post"});
   } else {
     const post = await postModel.findById(id);
     post.remove();
-    response.status(200).json({msg: "Post deleted successfully"});
+    return response.status(200).json({msg: "Post deleted successfully"});
   }
 });
 
 app.get('/',(request, response) =>{
-    response.status(200).sendFile(path.resolve(__dirname,'../frontend/login.html'))
+    return response.status(200).sendFile(path.resolve(__dirname,'../frontend/login.html'))
 })
 
-app.get("/user:id", async (request, response) => {
-  const id = request.params
-  const users = await userModel.find({id});
-  response.status(200).json(users);
-});
-
-app.get("/posts", async (request, response) => {
+app.get("/api/blitzchat/posts", async (request, response) => {
   const posts = await postModel.find({});
-  response.json(posts);
-
+  return response.json(posts);
 });
 
 app.listen(3000, () => {
